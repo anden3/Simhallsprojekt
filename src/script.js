915eb45af37a425bd3a19b7965740d16d9b56402
André Vennberg <andre.vennberg@gmail.com>
2016-01-23 18:47:44 +0100

    Made TextureAtlas work (:

--- src/script.js
+++ src/script.js
@@ -11,11 +11,29 @@ var width = window.innerWidth;
 var height = window.innerHeight;
 
 var stage = new Container();
-var renderer = new autoDetectRenderer(width, height);
+var renderer = new autoDetectRenderer(width, height, {antialias: false});
+
+var floor1, floor2, floor3, floor4;
+var wall1, wall2;
 
 renderer.autoResize = true;
 document.body.appendChild(renderer.view);
 
-function animate() {
+loader.add("resources/BathroomAtlas.json").load(setup);
+
+function setup() {
+    var id = PIXI.loader.resources["resources/BathroomAtlas.json"].textures;
+
+    wall1 = new PIXI.extras.TilingSprite(id['BathroomWall1.png'], width, 72);
+    stage.addChild(wall1);
+
+    floor1 = new PIXI.extras.TilingSprite(id['BathroomTile1.png'], width, height - 77);
+    floor1.y = 72;
+    stage.addChild(floor1);
+
+    wall2 = new PIXI.extras.TilingSprite(id['BathroomWall2.png'], width, 5);
+    wall2.y = height - 5;
+    stage.addChild(wall2);
+
     renderer.render(stage);
 }
